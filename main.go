e630718a2bd3b718585521233c74416a50cc8191
Don Mennerich <don.mennerich@nyu.edu>
2024-03-06 08:33:56 -0500

    removing models, adding gitignore

--- main.go
+++ main.go
@@ -21,6 +21,27 @@ var (
 	transferInfo TransferInfo
 )
 
+type DC struct {
+	Title    string `json:"title"`
+	IsPartOf string `json:"is_part_of"`
+}
+
+type TransferInfo struct {
+	ContactName              string `yaml:"Contact-Name"`
+	ContactPhone             string `yaml:"Contact-Phone"`
+	ContactEmail             string `yaml:"Contact-Email"`
+	InternalSenderIdentifier string `yaml:"Internal-Sender-Identifier"`
+	OrganizationAdress       string `yaml:"OrganizationAddress"`
+	SourceOrganization       string `yaml:"SourceOrganization"`
+	ArchivesSpaceResourceURL string `yaml:"nyu-dl-archivesspace-resource-url"`
+	ResourceID               string `yaml:"nyu-dl-resource-id"`
+	ResourceTitle            string `yaml:"nyu-dl-resource-title"`
+	ContrentType             string `yaml:"nyu-dl-content-type"`
+	ContentClassification    string `yaml:"nyu-dl-content-classification"`
+	ProjectName              string `yaml:"nyu-dl-project-name"`
+	RStarCollectionID        string `yaml:"nyu-dl-rstar-collection-id"`
+}
+
 func init() {
 	flag.StringVar(&source, "source", "", "")
 	flag.StringVar(&stagingLoc, "staging", "", "")

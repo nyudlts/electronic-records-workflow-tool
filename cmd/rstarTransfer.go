b34552b411ca991fccaca618c379ef7bda8d0fba
Don Mennerich <don.mennerich@nyu.edu>
2024-10-16 10:59:28 -0400

    adding rstar transfer

--- cmd/rstarTransfer.go
+++ cmd/rstarTransfer.go
@@ -1,32 +1,28 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
-	"regexp"
 
-	bagit "github.com/nyudlts/go-bagit"
 	"github.com/spf13/cobra"
 )
 
 func init() {
 	validateERsCmd.Flags().StringVar(&ersLoc, "staging-location", "", "")
 	validateERsCmd.Flags().StringVar(&ersRegex, "regexp", "", "")
-	rootCmd.AddCommand(validateERsCmd)
+	rootCmd.AddCommand(rstarXfrCmd)
 }
-
-var validateERsCmd = &cobra.Command{
-	Use: "validate-ers",
+var rstarXfrCmd = &cobra.Command{
+	Use: "transfer",
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := validateERs(); err != nil {
+		if err := transferToRstar(); err != nil {
 			panic(err)
 		}
-		fmt.Println("All ER bags are valid")
 	},
 }
 
-func validateERs() error {
+func transferToRstar() err {
 	ers, err := os.Stat(ersLoc)
 	if err != nil {
 		return err
@@ -49,18 +45,14 @@ func validateERs() error {
 
 	for _, entry := range directoryEntries {
 		if entry.IsDir() && ersPtn.MatchString(entry.Name()) {
-			fmt.Printf("fast validating %s\n", entry.Name())
-			erPath := filepath.Join(ersLoc, entry.Name())
-			bag, err := bagit.GetExistingBag(erPath)
-			if err != nil {
-				return err
-			}
-
-			if err := bag.ValidateBag(true, false); err != nil {
+			fmt.Println("transferring", entry.Name())
+			xferBag := filepath.Join(ersLoc,entry.Name)
+			xferCmd := exec.Command("rstar-scp.exp", xferBag)
+			xferCmd.Stdout = os.Stdout
+			if err := xferCmd.Run(); err != nil {
 				return err
 			}
 		}
-	}
 
 	return nil
 }

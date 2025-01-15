8d35ce89fa12252ddc6483d7d557eacc804a1794
Don Mennerich <don.mennerich@nyu.edu>
2025-01-15 08:59:01 -0500

    adding acm transfer cmd

---
+++ cmd/acmbdTransfer.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
+
+var sourceLocation string
+var stagingLocation string
+var collectionCode string
+
+func init() {
+	acmbdXferCmd.Flags().StringVar(&sourceLocation, "source", "", "Source location")
+	acmbdXferCmd.Flags().StringVar(&stagingLocation, "staging", "", "Staging location")
+	acmbdXferCmd.Flags().StringVar(&collectionCode, "collection-code", "", "Collection code")
+	rootCmd.AddCommand(acmbdXferCmd)
+}
+
+var acmbdXferCmd = &cobra.Command{
+	Use: "transfer-acm",
+	Run: func(cmd *cobra.Command, args []string) {
+		transferACM()
+	},
+}
+
+func transferACM() error {
+
+	//create the logfile
+	logFileName := fmt.Sprintf("%s-adoc-acm-transfer.txt", collectionCode)
+	logFile, err := os.Create(logFileName)
+	if err != nil {
+		return err
+	}
+	writer := bufio.NewWriter(logFile)
+	defer logFile.Close()
+
+	targetDir := filepath.Join(stagingLocation, collectionCode)
+	cmd := exec.Command("rsync", "-av", sourceLocation, targetDir)
+	cmd.Stdout = os.Stdout
+
+	b, err := cmd.CombinedOutput()
+	if err != nil {
+		return nil
+	}
+	if _, err := writer.Write(b); err != nil {
+		return err
+	}
+
+	return nil
+}

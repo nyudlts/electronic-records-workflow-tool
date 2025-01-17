e625c069378891324491e007f9539dc7d9c45963
Don Mennerich <don.mennerich@nyu.edu>
2025-01-17 17:04:31 -0500

    updating init

--- cmd/init.go
+++ cmd/init.go
@@ -14,13 +14,13 @@ type AdocConfig struct {
 	StagingLoc     string `yaml:"staging-location"`
 	XferLoc        string `yaml:"xfer-location"`
 	PartnerCode    string `yaml:"partner-code"`
-	CollectionCall string `yaml:"collection-call"`
+	CollectionCode string `yaml:"collection-code"`
 	ProjectLoc     string `yaml:"project-location"`
 }
 
 func init() {
-	initCmd.Flags().StringVar(&partnerCode, "partner-code", "", "the partner code to use for the adoc")
-	initCmd.Flags().StringVar(&collectionCall, "collection-call", "", "the collection call no. to use for the adoc")
+	initCmd.Flags().StringVarP(&partnerCode, "partner-code", "p", "", "the partner code to use adoc")
+	initCmd.Flags().StringVarP(&collectionCode, "collection-code", "c", "", "the collection code to use for adoc")
 	rootCmd.AddCommand(initCmd)
 }
 
@@ -35,11 +35,13 @@ var initCmd = &cobra.Command{
 
 func initAdoc() error {
 	var err error
+	//load the configuration
 	adocConfig, err = loadConfig()
 	if err != nil {
 		return err
 	}
 
+	//create default project directories
 	if err := mkProjectDir(); err != nil {
 		return err
 	}
@@ -47,44 +49,55 @@ func initAdoc() error {
 }
 
 func loadConfig() (*AdocConfig, error) {
+	//read the adoc-config
 	b, err := vfs.ReadFile("adoc-config.yml")
 	if err != nil {
 		return nil, err
 	}
+
+	//unmarshal to config options
 	config := &AdocConfig{}
 	if err := yaml.Unmarshal(b, config); err != nil {
 		return nil, err
 	}
 
+	//update members
 	config.PartnerCode = partnerCode
-	config.StagingLoc = filepath.Join(config.StagingLoc, partnerCode, collectionCall)
-	config.CollectionCall = collectionCall
-	config.ProjectLoc = filepath.Join(config.ProjectLoc, config.CollectionCall)
+	config.StagingLoc = filepath.Join(config.StagingLoc, partnerCode, collectionCode)
+	config.CollectionCode = collectionCode
+	config.ProjectLoc = filepath.Join(config.ProjectLoc, config.CollectionCode)
+
 	return config, nil
 }
 
 func mkProjectDir() error {
+	//create the project directory
 	if err := os.Mkdir(adocConfig.ProjectLoc, 0775); err != nil {
 		return err
 	}
 
-	if err := os.Mkdir(filepath.Join(adocConfig.ProjectLoc, adocConfig.PartnerCode, "ers"), 0775); err != nil {
+	//create the aips directory
+	if err := os.Mkdir(filepath.Join(adocConfig.ProjectLoc, adocConfig.PartnerCode, "aips"), 0775); err != nil {
 		return err
 	}
 
+	//create the logs directory
 	if err := os.Mkdir(filepath.Join(adocConfig.ProjectLoc, adocConfig.PartnerCode, "logs"), 0775); err != nil {
 		return err
 	}
 
+	//create the resync output directory
 	if err := os.Mkdir(filepath.Join(adocConfig.ProjectLoc, adocConfig.PartnerCode, "rsync"), 0775); err != nil {
 		return err
 	}
 
+	//marshall the updated config
 	b, err := yaml.Marshal(adocConfig)
 	if err != nil {
 		return err
 	}
 
+	//write the config to the project directory
 	if err := os.WriteFile(filepath.Join(adocConfig.ProjectLoc, "config.yml"), b, 0755); err != nil {
 		return err
 	}

074619ccb4e07d891e864fb662bc7341665ab6f0
Don Mennerich <don.mennerich@nyu.edu>
2025-02-11 09:41:26 -0500

    updating validate-sip

--- cmd/validateSIP.go
+++ cmd/validateSIP.go
@@ -8,6 +8,7 @@ import (
 	"regexp"
 	"sort"
 
+	"github.com/nyudlts/go-aspace"
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v2"
 )
@@ -15,6 +16,9 @@ import (
 func init() {
 	rootCmd.AddCommand(validateCmd)
 }
+
+var workOrder aspace.WorkOrder
+var row aspace.WorkOrderRow
 
 var validateCmd = &cobra.Command{
 	Use:   "validate-sip",
@@ -48,74 +52,86 @@ var validateCmd = &cobra.Command{
 
 func validate() error {
 	//check that the source directory exists
-	fmt.Print("  1. checking that source location exists and is a directory: ")
+	fmt.Print("  1. checking that source location exists: ")
 	fileInfo, err := os.Stat(adocConfig.StagingLoc)
 	if err != nil {
 		log.Printf("[ERROR] %s\n", err.Error())
-		return err
-	}
+		fmt.Printf("source location %s does not exist", adocConfig.StagingLoc)
+	} else {
 
-	if !fileInfo.IsDir() {
-		log.Printf("[ERROR] %s is not a directory\n", adocConfig.StagingLoc)
-		return fmt.Errorf("source location is not a directory")
+		if !fileInfo.IsDir() {
+			log.Printf("[ERROR] %s is not a directory\n", adocConfig.StagingLoc)
+			fmt.Printf("source location %s is not a directory", adocConfig.StagingLoc)
+		} else {
+			log.Printf("[INFO] check 1. %s exists and is a directory\n", adocConfig.StagingLoc)
+			fmt.Println("OK")
+		}
 	}
-	fmt.Println("OK")
-	log.Printf("[INFO] check 1. %s exists and is a directory\n", adocConfig.StagingLoc)
 
 	//check that there is a metadata directory
 	fmt.Print("  2. checking that source directory contains a metadata directory: ")
 	mdDirLocation := filepath.Join(adocConfig.StagingLoc, "metadata")
 	mdDir, err := os.Stat(mdDirLocation)
 	if err != nil {
-		return err
-	}
+		fmt.Printf("source location %s does not contain a metadata directory", mdDirLocation)
+		log.Printf("[ERROR] %s does not contain a metadata directory\n", adocConfig.StagingLoc)
+	} else {
 
-	if !mdDir.IsDir() {
-		return fmt.Errorf("source metadata location is not a directory")
+		if !mdDir.IsDir() {
+			fmt.Printf("%s metadata directory is not a directory\n", mdDirLocation)
+			log.Printf("[ERROR] %s is not a direcotry\n", mdDirLocation)
+		} else {
+			log.Printf("[INFO] check 2. %s contains a metadata directory\n", adocConfig.StagingLoc)
+			fmt.Println("OK")
+		}
 	}
-
-	log.Printf("[INFO] check 2. %s contains a metadata directory\n", adocConfig.StagingLoc)
-	fmt.Println("OK")
 
 	//check that a workOrder exists
 	fmt.Print("  3. checking that a valid workorder file exists: ")
 	workorderName, err := getWorkOrderFile(mdDirLocation)
 	if err != nil {
-		return err
+		fmt.Printf("metadata directory %s does not contain a work order\n", mdDirLocation)
+		log.Printf("[ERROR] metadata directory %s does not contain a work order\n", mdDirLocation)
+	} else {
+		//check that the workorder is valid
+		workOrder, err = parseWorkOrder(mdDirLocation, workorderName)
+		if err != nil {
+			fmt.Printf("work order %s is not valid: %s\n", mdDirLocation, err.Error())
+			log.Printf("[ERROR] work order %s is not valid: %s\n", mdDirLocation, err.Error())
+		} else {
+			fmt.Println("OK")
+			log.Printf("[INFO] check 3. %s contains a valid worker order \n", mdDirLocation)
+		}
 	}
-
-	//check that the workorder is valid
-	workOrder, err := parseWorkOrder(mdDirLocation, workorderName)
-	if err != nil {
-		return err
-	}
-	fmt.Println("OK")
-	log.Printf("[INFO] check 3. %s contains a valid worker order \n", mdDirLocation)
 
 	//check that a transfer info exists
 	fmt.Printf("  4. checking that %s contains a valid transfer-info.txt: ", mdDirLocation)
 	xferInfoLocation := filepath.Join(mdDirLocation, "transfer-info.txt")
 	_, err = os.Stat(xferInfoLocation)
 	if err != nil {
-		return err
+		fmt.Println("transfer-info.txt does not exist in metadata directory\n")
+		log.Println("[ERROR] transfer-info,txt does not exist in metadata directory")
+	} else {
+		xferBytes, err := os.ReadFile(xferInfoLocation)
+		if err != nil {
+			fmt.Printf("could not read transfer-info.txt: %s\n", xferInfoLocation)
+			log.Printf("[ERROR]could not read transfer-info.txt: %s\n", xferInfoLocation)
+		} else {
+			transferInfo := TransferInfo{}
+			if err := yaml.Unmarshal(xferBytes, &transferInfo); err != nil {
+				fmt.Println("could unmarshal transfer-info.txt")
+				log.Println("[ERROR] could not unmarshal transfer-info.txt")
+			} else {
+				if err := transferInfo.Validate(); err != nil {
+					fmt.Printf("transfer-info.txt is not valid: %s\n", err.Error())
+					log.Printf("[ERROR] transfer-info.txt is not valid: %s\n", err.Error())
+				} else {
+					log.Printf("[INFO] check 4. %s contains a valid transfer-info.txt \n", mdDirLocation)
+					fmt.Println("OK")
+				}
+			}
+		}
 	}
-
-	//validate transfer-info.txt
-	xferBytes, err := os.ReadFile(xferInfoLocation)
-	if err != nil {
-		return err
-	}
-	transferInfo := TransferInfo{}
-	if err := yaml.Unmarshal(xferBytes, &transferInfo); err != nil {
-		return err
-	}
-
-	//validate transfer-info.txt
-	if err := transferInfo.Validate(); err != nil {
-		return err
-	}
-	log.Printf("[INFO] check 4. %s contains a valid transfer-info.txt \n", mdDirLocation)
-	fmt.Println("OK")
 
 	//get a list of componentIDs from work order
 	fmt.Printf("  5. checking workorder %s for duplicate cuids: ", workorderName)
@@ -160,24 +176,26 @@ func validate() error {
 	fmt.Print("  7. checking that there no extra directories or files in source location: ")
 	sourceDirs, err := os.ReadDir(adocConfig.StagingLoc)
 	if err != nil {
-		panic(err)
-	}
+		log.Printf("[ERROR] duplicate componentID, %s, found in workorder", row.GetComponentID())
+		fmt.Printf("[ERROR] duplicate componentID, %s, found in workorder\n", row.GetComponentID())
+	} else {
 
-	extraDirs := 0
-	for _, sourceDir := range sourceDirs {
-		if sourceDir.Name() != "metadata" {
-			if !contains(sourceDir.Name(), componentIDs) {
-				extraDirs++
-				log.Printf("[ERROR] %s is not listed on workorder\n", sourceDir.Name())
+		extraDirs := 0
+		for _, sourceDir := range sourceDirs {
+			if sourceDir.Name() != "metadata" {
+				if !contains(sourceDir.Name(), componentIDs) {
+					extraDirs++
+					log.Printf("[ERROR] %s is not listed on workorder\n", sourceDir.Name())
+				}
 			}
 		}
-	}
 
-	log.Printf("[INFO] check 7. %s contained %d extra objects\n", adocConfig.StagingLoc, extraDirs)
-	if extraDirs > 0 {
-		fmt.Println("ERROR")
-	} else {
-		fmt.Println("OK")
+		log.Printf("[INFO] check 7. %s contained %d extra objects\n", adocConfig.StagingLoc, extraDirs)
+		if extraDirs > 0 {
+			fmt.Println("ERROR")
+		} else {
+			fmt.Println("OK")
+		}
 	}
 
 	//check that clamscan logs
@@ -187,35 +205,37 @@ func validate() error {
 	//check there are no failed clamscan logs
 	mdFiles, err := os.ReadDir(mdDirLocation)
 	if err != nil {
-		return err
-	}
+		log.Printf("[ERROR] cannot open metadata directory: %s\n", mdDirLocation)
+		fmt.Printf("cannot open metadata directory: %s\n", mdDirLocation)
+	} else {
 
-	clamInfectedPtn := regexp.MustCompile("\nInfected files: 0")
-	failedClamScans := 0
-	for _, mdFile := range mdFiles {
-		if clamscanLogPtn.MatchString(mdFile.Name()) {
-			fileBytes, err := os.ReadFile(filepath.Join(mdDirLocation, mdFile.Name()))
-			if err != nil {
-				return err
+		clamInfectedPtn := regexp.MustCompile("\nInfected files: 0")
+		failedClamScans := 0
+		for _, mdFile := range mdFiles {
+			if clamscanLogPtn.MatchString(mdFile.Name()) {
+				fileBytes, err := os.ReadFile(filepath.Join(mdDirLocation, mdFile.Name()))
+				if err != nil {
+					log.Printf("[ERROR] cannot read clamscan log: %s", mdFile.Name())
+					fmt.Printf("cannot read clamscan log: %s\n", mdFile.Name())
+				} else {
+					if !clamInfectedPtn.Match(fileBytes) {
+						failedClamScans++
+						log.Printf("[ERROR] clamscan %s contained infected files", mdFile.Name())
+					}
+				}
 			}
-			if !clamInfectedPtn.Match(fileBytes) {
-				failedClamScans++
-				log.Printf("[ERROR] %s reports infected files\n", mdFile.Name())
-			}
+		}
+
+		log.Printf("[INFO] check 8. %s contained %d failed clamscan scans", adocConfig.StagingLoc, failedClamScans)
+
+		if failedClamScans > 0 {
+			fmt.Println("ERROR")
+		} else {
+			fmt.Println("OK")
 		}
 	}
 
-	log.Printf("[INFO] check 8. %s contained %d failed clamscan scans", adocConfig.StagingLoc, failedClamScans)
-
-	if failedClamScans > 0 {
-		fmt.Println("ERROR")
-	} else {
-		fmt.Println("OK")
-	}
-
-	//validation complete
 	return nil
-
 }
 
 func contains(s string, sl []string) bool {

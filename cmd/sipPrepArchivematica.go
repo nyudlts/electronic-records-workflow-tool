9e66d19028722f6e5de42a17de5b5551498faa65
Don Mennerich <don.mennerich@nyu.edu>
2025-03-03 10:31:03 -0500

    moving models to models.go

--- cmd/sipPrepArchivematica.go
+++ cmd/sipPrepArchivematica.go
@@ -20,30 +20,6 @@ var (
 	infectedFilesPtn = regexp.MustCompile("\nInfected files: 0\n")
 )
 
-type DC struct {
-	Title    string `json:"title"`
-	IsPartOf string `json:"is_part_of"`
-}
-
-type TransferInfo struct {
-	ContactName              string `yaml:"Contact-Name"`
-	ContactPhone             string `yaml:"Contact-Phone"`
-	ContactEmail             string `yaml:"Contact-Email"`
-	InternalSenderIdentifier string `yaml:"Internal-Sender-Identifier"`
-	OrganizationAddress      string `yaml:"Organization-Address"`
-	SourceOrganization       string `yaml:"Source-Organization"`
-	ArchivesSpaceResourceURL string `yaml:"nyu-dl-archivesspace-resource-url"`
-	ResourceID               string `yaml:"nyu-dl-resource-id"`
-	ResourceTitle            string `yaml:"nyu-dl-resource-title"`
-	ContentType              string `yaml:"nyu-dl-content-type"`
-	ContentClassification    string `yaml:"nyu-dl-content-classification"`
-	ProjectName              string `yaml:"nyu-dl-project-name"`
-	RStarCollectionID        string `yaml:"nyu-dl-rstar-collection-id"`
-	PackageFormat            string `yaml:"nyu-dl-package-format"`
-	UseStatement             string `yaml:"nyu-dl-use-statement"`
-	TransferType             string `yaml:"nyu-dl-transfer-type"`
-}
-
 func ProcessWorkOrderRows(p Params, numWorkers int) ([][]string, error) {
 	params = p
 

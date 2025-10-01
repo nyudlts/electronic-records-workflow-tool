c7c0e4738940227013a75bf18d65c1e1a2731faf
Donald Mennerich <don.mennerich@nyu.edu>
2025-10-01 15:51:10 -0400

    updating package update

--- lib/rstar.go
+++ lib/rstar.go
@@ -7,8 +7,16 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"runtime"
 	"strings"
+
+	bagit "github.com/nyudlts/go-bagit"
+)
+
+var (
+	woMatcher = regexp.MustCompile("aspace_wo.tsv$")
+	tiMatcher = regexp.MustCompile("transfer-info.txt")
 )
 
 func PrintRStarPackageSize(directories bool) error {
@@ -81,7 +89,7 @@ func PrepareRStarPackages() error {
 			return fmt.Errorf("preparing package %s failed: %v", fi.Name(), err)
 		}
 	}
-	fmt.Println("  *rstar package prep complete")
+	fmt.Println("  * rstar package prep complete")
 	return nil
 }
 
@@ -129,7 +137,7 @@ func prepAmaticaAIP(amaticaAIPLocation string) error {
 	}
 	aipStageLoc := filepath.Join(config.AIPLoc, fi.Name())
 	msg := fmt.Sprintf("copying %s to aip directory", fi.Name())
-	fmt.Printf("  * %s\n", msg)
+	fmt.Printf("    * %s\n", msg)
 	log.Printf("[INFO] %s", msg)
 	var cmd *exec.Cmd
 	var out []byte
@@ -140,7 +148,7 @@ func prepAmaticaAIP(amaticaAIPLocation string) error {
 			return err
 		}
 	} else {
-		cmd = exec.Command("rsync", "-rav", amaticaAIPLocation, aipStageLoc)
+		cmd = exec.Command("rsync", "-rav", amaticaAIPLocation, config.AIPLoc)
 		out, err = cmd.CombinedOutput()
 		if err != nil {
 			fmt.Println()
@@ -153,17 +161,167 @@ func prepAmaticaAIP(amaticaAIPLocation string) error {
 		return err
 	}
 
-	msg = fmt.Sprintf("  * Updating package %s", fi.Name())
+	msg = fmt.Sprintf("    * Updating package %s", fi.Name())
 	fmt.Println(msg)
 	log.Printf("[INFO] %s", msg)
 
-	if err := updatePackage(aipStageLoc, config.ProjectLoc); err != nil {
+	if err := updatePackage(aipStageLoc); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func updatePackage(aipStageLoc, tmpLoc string) error {
+func updatePackage(bagLocation string) error {
+
+	fmt.Println("      * opening bag", filepath.Base(bagLocation))
+	bag, err := bagit.GetExistingBag(bagLocation)
+	if err != nil {
+		return err
+	}
+
+	//validate the bag
+	fmt.Printf("      * Validating bag %s\n", filepath.Base(bagLocation))
+	if err := bag.ValidateBag(false, false); err != nil {
+		return err
+	}
+
+	//locate the work order
+	fmt.Println("      * Locating work order")
+	matches := bag.Payload.FindFilesInPayload(woMatcher)
+	if len(matches) != 1 {
+		return fmt.Errorf("no workorder found")
+	}
+	woPath := matches[0].Path
+
+	//move the work order to the bag's root
+	fmt.Println("      * Moving work order to bag's root")
+	if err := bag.AddFileToBagRoot(woPath); err != nil {
+		return err
+	}
+
+	//locate the transfer-info.txt
+	fmt.Println("      * Locating transfer-info.txt")
+	matches = bag.Payload.FindFilesInPayload(tiMatcher)
+	if len(matches) != 1 {
+		return fmt.Errorf("no transfer-info.txt found")
+	}
+	tiPath := matches[0].Path
+	tiPath = strings.ReplaceAll(tiPath+"/", bagLocation, "")
+
+	//create a tag set from transfer-info.txt
+	fmt.Println("      * Creating new tag set from transfer-info.txt")
+	transferInfo, err := bagit.NewTagSet(tiPath, bagLocation)
+	if err != nil {
+		return err
+	}
+
+	//Update the hostname
+	fmt.Println("      * Adding hostname to tag set: ")
+	hostname, err := os.Hostname()
+	if err != nil {
+		return err
+	}
+	transferInfo.Tags["nyu-dl-hostname"] = hostname
+
+	//add pathname to the tag-set
+	fmt.Println("      * Adding bag's path to tag set")
+	path, err := filepath.Abs(bagLocation)
+	if err != nil {
+		return err
+	}
+	path, err = filepath.EvalSymlinks(path)
+	if err != nil {
+		return err
+	}
+	transferInfo.Tags["nyu-dl-pathname"] = path
+
+	//backup bag-info
+	fmt.Println("      * Backing up bag-info.txt")
+	bagInfoLocation := filepath.Join(bagLocation, "bag-info.txt")
+	backupLocation := filepath.Join(config.ProjectLoc, ".bag-info.txt")
+
+	biBytes, err := os.ReadFile(bagInfoLocation)
+	if err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(backupLocation, biBytes, 0777); err != nil {
+		return err
+	}
+
+	//getting tagset from bag-info
+	fmt.Println("      * Creating new tag set from bag-info.txt")
+	bagInfo, err := bagit.NewTagSet("bag-info.txt", bagLocation)
+	if err != nil {
+		return err
+	}
+
+	//merge tagsets
+	fmt.Println("      * Merging Tag Sets")
+	bagInfo.AddTags(transferInfo.Tags)
+	bagInfoBytes := bagInfo.GetTagSetAsByteSlice()
+
+	//write new bag-info.txt
+	fmt.Println("      * Rewriting bag-info.txt")
+	if err := os.WriteFile(bagInfoLocation, bagInfoBytes, 0777); err != nil {
+		return err
+	}
+
+	/*
+
+		fmt.Printf("  * Opening bag-info.txt: ")
+		bagInfoFile, err := os.Open(bagInfoLocation)
+		if err != nil {
+			return err
+		}
+		defer bagInfoFile.Close()
+		fmt.Printf("OK\n")
+
+		fmt.Printf("  * Rewriting bag-info.txt: ")
+		if err := os.WriteFile(bagInfoLocation, bagInfoBytes, 0777); err != nil {
+			return err
+		}
+		fmt.Printf("OK\n")
+
+		//create new manifest object for tagmanifest-sha256.txt
+		fmt.Printf("  * Creating new tagmanifest-sha256.txt: ")
+		tagManifest, err := bagit.NewManifest(bagLocation, "tagmanifest-sha256.txt")
+		if err != nil {
+			return err
+		}
+		fmt.Printf("OK\n")
+
+		//update the checksum for bag-info.txt
+		fmt.Printf("  * Updating checksum for bag-info.txt in tagmanifest-sha256.txt: ")
+		if err := tagManifest.UpdateManifest("bag-info.txt"); err != nil {
+			return err
+		}
+		fmt.Printf("OK\n")
+
+		fmt.Printf("  * Rewriting tagmanifest-sha256.txt: ")
+		if err := tagManifest.Serialize(); err != nil {
+			return err
+		}
+		fmt.Printf("OK\n")
+
+		//validate the updated bag
+		fmt.Printf("\nValidating the updated bag: ")
+		if err := bag.ValidateBag(false, false); err != nil {
+			return err
+		}
+		fmt.Printf("OK\n")
+
+		//delete the backup bag-info
+		fmt.Printf("Deleting backup bag-info.txt: ")
+		if err := os.Remove(backupLocation); err != nil {
+			return err
+		}
+		fmt.Printf("OK\n")
+
+
+	*/
+	fmt.Println("    * Package preparation complete")
+	fmt.Println()
 	return nil
 }
